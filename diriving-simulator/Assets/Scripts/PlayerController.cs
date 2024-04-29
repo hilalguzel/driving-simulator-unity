e294aadd7c6a5ad30fc71bf5ec48eec03475ce7d
Hilal Güzel <guzelhilal23@gmail.com>
2024-04-29 13:42:03 +0300

    player controller updated
    Vector3 defined for forward

--- diriving-simulator/Assets/Scripts/PlayerController.cs
+++ diriving-simulator/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //We will move the vehicle forward
-        transform.Translate(0,0,1);
+        // transform.Translate(0,0,1);
+
+        transform.Translate(Vector3.forward); //same function as above line of code
     }
 }
